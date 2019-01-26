f248fe63da215d63d816be04744813cee3faeae5
Winston Liu <50Wliu@users.noreply.github.com>
2019-01-26 12:58:01 -0500

    Tabs to spaces

--- grammar.js
+++ grammar.js
@@ -2,9 +2,9 @@ module.exports = grammar({
   name: 'jlex',
 
   extras: $ => [
-		$.comment,
-		/[\s\uFEFF\u2060\u200B\u00A0]/, // whitespace
-	],
+    $.comment,
+    /[\s\uFEFF\u2060\u200B\u00A0]/, // whitespace
+  ],
 
   rules: {
     source_file: $ => seq(
@@ -88,21 +88,21 @@ module.exports = grammar({
     java_code: $ => 'hello', // TODO
 
     comment: $ => token(choice(
-			/\/\/.*/, // slash comment
-			// http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
-			seq(
-				'/*',
-				/[^*]*\*+([^/*][^*]*\*+)*/,
-				'/',
-			),
-		)),
+      /\/\/.*/, // slash comment
+      // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
+      seq(
+        '/*',
+        /[^*]*\*+([^/*][^*]*\*+)*/,
+        '/',
+      ),
+    )),
   }
 });
 
 function repeatWithDelimiter1 (rule, delimiter) {
-	return seq(rule, repeat(seq(delimiter, rule)));
+  return seq(rule, repeat(seq(delimiter, rule)));
 }
 
 function repeatWithDelimiter (rule, delimiter) {
-	return optional(repeatWithDelimiter1(rule, delimiter));
+  return optional(repeatWithDelimiter1(rule, delimiter));
 }
